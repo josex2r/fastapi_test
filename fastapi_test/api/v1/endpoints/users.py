66850ac3a0f5b501d6890a51d24049be6b18ce70
josex2r <jlrepresa@gmail.com>
2022-03-17 10:19:45 +0100

    refactor: remove unused var

--- fastapi_test/api/v1/endpoints/users.py
+++ fastapi_test/api/v1/endpoints/users.py
@@ -7,23 +7,6 @@ from fastapi_test.crud.user import CRUDUser
 
 router = APIRouter()
 
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "fakehashedsecret",
-        "disabled": False,
-    },
-    "alice": {
-        "username": "alice",
-        "full_name": "Alice Wonderson",
-        "email": "alice@example.com",
-        "hashed_password": "fakehashedsecret2",
-        "disabled": True,
-    },
-}
-
 
 @router.get("/me")
 async def read_users_me(current_user: User = Depends(get_current_user)):
